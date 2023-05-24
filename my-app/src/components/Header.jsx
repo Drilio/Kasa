19a4064b97aa9096773532b6b1552334d6d7c99e
Antoine ROY <antoineroy92@gmail.com>
2023-05-24 14:43:47 +0200

    avancer sur la banner

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -5,34 +5,27 @@ import s from '../images/S.png'
 import a from '../images/a.png'
 import { Link } from 'react-router-dom'
 
-
-function Logo() {
-    return (
-        <div className='img-header'>
-            <img src={k} alt="K"></img>
-            <img src={logo} alt="logo"></img>
-            <img src={s} alt="s"></img>
-            <img src={a} alt="a"></img>
-        </div>
-    )
-}
-
-function Navigation() {
-    return (
-        <nav>
-            <ul>
-                <li><Link to="/" >Accueil</Link></li>
-                <li><Link to="/A-Propos" >A Propos</Link></li>
-            </ul>
-        </nav>
-    )
-}
-
 export default function Header() {
+
     return (
         <div className="header">
-            <Logo />
-            <Navigation />
+            <div className="top-header">
+                <div className='img-header'>
+                    <img src={k} alt="K"></img>
+                    <img src={logo} alt="logo"></img>
+                    <img src={s} alt="s"></img>
+                    <img src={a} alt="a"></img>
+                </div>
+                <nav>
+                    <ul>
+                        <li><Link to="/" >Accueil</Link></li>
+                        <li><Link to="/A-Propos" >A Propos</Link></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="banner">
+                <h1 id="title-banner">Chez vous, partout et ailleurs</h1>
+            </div>
         </div>
     )
 }
